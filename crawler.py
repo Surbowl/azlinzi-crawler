404158d70dd8447499d710322f730f25c99f163b
碗碗 <43742453+Surbowl@users.noreply.github.com>
2020-01-31 09:08:32 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import random
 import time
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,16 +8,17 @@ from bs4 import BeautifulSoup
 uri = "https://azlinzi.wordpress.com/2019/04/05/%E4%BE%8D%E5%BA%94%E7%94%9F%E6%89%BF%E8%B1%AA/"
 
 #Save path
+#If you save on disk C, you may need administrator privileges
 filePath = "D:/azlinzi"
 
 #Time interval of crawler
-sleepTime = 0
+sleepTime = 1
 
 #Connect & read timeout
 timeOut=(10, 60)
 
 
-uaList = [
+ua_list = [
             {"User-Agent":"Opera/9.27 (Windows NT 5.2; U; zh-cn)"},
             {"User-Agent":"Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"},
             {"User-Agent":"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; 360se)"},
@@ -27,33 +29,38 @@ uaList = [
 
 if __name__ == "__main__":
     print("Start")
-    response = requests.get(uri, headers = random.choice(uaList))
+    #Get html page
+    response = requests.get(uri, headers = random.choice(ua_list))
     if(response.status_code == 200):
+        #Find all img
         soup = BeautifulSoup(response.content, "lxml")
         soup = soup.find("div", class_="tiled-gallery")
         soup = soup.find_all("img")
-
         total = len(soup)
-        number = 1
-        print("There are %d pictures, please wait a moment ..."%total)
-
-        for imgHtml in soup:
-            time.sleep(sleepTime)
-            print("%d/%d"%(number, total), end='\t')
-            #Get img src
-            imgSrc = imgHtml.get("data-orig-file")
-            fileName = imgSrc.split('/')[-1]
-            print(imgSrc, end='\t')
-            try:
-                #Get img
-                imgFile = requests.get(imgSrc, headers = random.choice(uaList), timeout = timeOut).content
-                #Save file
-                with open(filePath + '/' + fileName, "wb") as f:
-                    f.write(imgFile)
-                print("Succeed")
-            except:
-                print("Fail")
-            number = number + 1
+        if(total > 0):
+            print("There are %d pictures, please wait a moment..."%total)
+            #Make dirs
+            if not os.path.exists(filePath):
+                os.makedirs(filePath)
+            #Start crawling for images
+            number = 1
+            for imgHtml in soup:
+                time.sleep(sleepTime)
+                print("%d/%d"%(number, total), end='\t')
+                #Get img src
+                imgSrc = imgHtml.get("data-orig-file")
+                fileName = imgSrc.split('/')[-1]
+                print(imgSrc, end='\t')
+                try:
+                    #Get img
+                    imgFile = requests.get(imgSrc, headers = random.choice(ua_list), timeout = timeOut).content
+                    #Save file
+                    with open(filePath + '/' + fileName,'wb') as f:
+                        f.write(imgFile)
+                    print("Succeed")
+                except:
+                    print("Fail")
+                number = number + 1
         print("Finish")
     else:
         print("Websites inaccessible")
